6d53a5ba9294d5f6ae7a196685d255b14b03e16f
Cedric Nicoloso <cedric.nicoloso@gmail.com>
2018-06-26 09:27:08 +0200

    Use a proper if for debug level log

--- server.js
+++ server.js
@@ -3,5 +3,8 @@ const app = require('./src/app')
 
 const port = process.env.PORT || 3000
 app.listen(port)
+
 log.info(`Server listening on ${port}...`)
-process.env.LOG_LEVEL === 'debug' && log.debug(`Debug activated`)
+if (process.env.LOG_LEVEL === 'debug') {
+  log.debug(`Debug activated`)
+}
