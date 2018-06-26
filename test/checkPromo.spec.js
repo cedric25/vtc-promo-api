9b602c52a33f54363a8f870494e483637414523c
Cedric Nicoloso <cedric.nicoloso@gmail.com>
2018-06-26 12:44:26 +0200

    Fix supertest test with proper payload

--- test/checkPromo.spec.js
+++ test/checkPromo.spec.js
@@ -1,3 +1,6 @@
+// Run with:
+// npx mocha test/checkPromo.spec.js
+
 const { assert } = require('chai')
 const request = require('supertest')
 const app = require('../src/app')
@@ -10,10 +13,31 @@ describe('POST /booking-promo route', () => {
   })
 
   describe('When calling with no payload', () => {
-    it('Should answer with a result of true', async () => {
-      const response = await request(app).post('/booking-promo')
-      assert.equal(response.statusCode, 200)
-      assert.deepEqual(response.body, { result: true })
+    it('Should answer with a result of true', done => {
+      const requestBody = {
+        'promocode_name': 'WeatherCode',
+        'arguments': {
+          'age': 25,
+          'meteo': {
+            'town': 'Lyon'
+          }
+        }
+      }
+      request(app)
+        .post('/booking-promo')
+        .send(requestBody)
+        .expect(200)
+        .end((err, res) => {
+          if (err) return done(err)
+          assert.deepEqual(res.body, {
+            'promocode_name': 'WeatherCode',
+            'status': 'accepted',
+            'avantage': {
+              'percent': 20
+            }
+          })
+          done()
+        })
     })
   })
 })
