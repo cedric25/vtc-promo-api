75fb872be71d0701ddecf45354aa04143e9ec4c3
Cedric Nicoloso <cedric.nicoloso@gmail.com>
2018-06-26 12:29:32 +0200

    Meteo: Missing 'http' + JSON.parse

--- src/external/meteo.js
+++ src/external/meteo.js
@@ -4,24 +4,25 @@ const log = require('../logger')
 const API_KEY = 'd0562f476913da692a065c608d0539f6'
 
 function buildWeatherUrl (endpoint) {
-  return `api.openweathermap.org/data/2.5${endpoint}&units=metric&APPID=${API_KEY}`
+  return `http://api.openweathermap.org/data/2.5${endpoint}&units=metric&APPID=${API_KEY}`
 }
 
 async function getWeatherInTown (cityName) {
   const url = buildWeatherUrl(`/weather?q=${cityName}`)
   try {
     const weatherAnswer = await rp(url)
-    console.log('weatherAnswer', weatherAnswer)
-    return buildMeteoAnswer(weatherAnswer)
+    const weatherJsonAnswer = JSON.parse(weatherAnswer)
+    log.debug('weatherAnswer', weatherJsonAnswer)
+    return buildMeteoAnswer(weatherJsonAnswer)
   } catch (err) {
     log.error('Fail to call weather API...', err)
   }
 }
 
-function buildMeteoAnswer (apiAnswer) {
+function buildMeteoAnswer (weatherJsonAnswer) {
   return {
-    skyState: apiAnswer.weather[0].main.toLowerCase(),
-    temperature: apiAnswer.main.temp,
+    skyState: weatherJsonAnswer.weather[0].main.toLowerCase(),
+    temperature: weatherJsonAnswer.main.temp,
   }
 }
 
